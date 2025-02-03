61e999b0119bf34a325db1ab68dbd5a87cc49291
ChangedRuby <gugasboy7@gmail.com>
2025-02-03 18:51:19 -0300

    Added sliders to select the row and collumn count, not yet functional.

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,14 +1,21 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
-public class MainScreen implements ActionListener {
+public class MainScreen implements ActionListener, ChangeListener {
 
-    public JFrame frame = new JFrame();
-    public GridBagConstraints c = new GridBagConstraints();
-    public JButton myButton = new JButton("Start");
-    public JLabel myLabel = new JLabel("Minesweeper");
+    private JFrame frame = new JFrame();
+    private GridBagConstraints c = new GridBagConstraints();
+    private JButton myButton = new JButton("Start");
+    private JLabel myLabel = new JLabel("Minesweeper");
+    public JSlider rSlider = new JSlider(JSlider.HORIZONTAL,5,100,20);
+    public JSlider cSlider = new JSlider(JSlider.HORIZONTAL,5,100,20);
+    public JLabel rowLabel = new JLabel();
+    public JLabel colLabel = new JLabel();
 
     public MainScreen() {
 
@@ -19,7 +26,36 @@ public class MainScreen implements ActionListener {
 
         myLabel.setFont(new Font("Arial", Font.BOLD, 24));
         myLabel.setHorizontalAlignment(JLabel.CENTER);
-        myLabel.setPreferredSize(new Dimension(200,200));
+        myLabel.setPreferredSize(new Dimension(200,100));
+
+        rSlider.setPaintTicks(true);
+        rSlider.setPaintLabels(true);
+        rSlider.setPaintTrack(true);
+        cSlider.setPaintTicks(true);
+        cSlider.setPaintLabels(true);
+        cSlider.setPaintTrack(true);
+
+        rSlider.setMajorTickSpacing(95);
+        rSlider.setMinorTickSpacing(5);
+        cSlider.setMajorTickSpacing(95);
+        cSlider.setMinorTickSpacing(5);
+        //rSlider.setPreferredSize(new Dimension(1000,200));
+        //cSlider.setPreferredSize(new Dimension(1000,200));
+        //myButton.setPreferredSize(new Dimension(200,100));
+
+        Hashtable labelTable = new Hashtable();
+        labelTable.put(5, new JLabel("5"));
+        labelTable.put(50, new JLabel("50"));
+        labelTable.put(100, new JLabel("100"));
+        rSlider.setLabelTable(labelTable);
+        cSlider.setLabelTable(labelTable);
+
+        rSlider.addChangeListener(this);
+
+        rowLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        rowLabel.setPreferredSize(new Dimension(1000,10));
+        rowLabel.setSize(1000,10);
+        rowLabel.setHorizontalAlignment(JLabel.LEFT);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Minesweeper");
@@ -38,9 +74,13 @@ public class MainScreen implements ActionListener {
         c.anchor = GridBagConstraints.CENTER;
 
         frame.add(myLabel, c);
-
         c.gridy = 1;
-
+        //frame.add(rowLabel, c);
+        c.gridy = 1;
+        frame.add(rSlider, c);
+        c.gridy = 2;
+        frame.add(cSlider, c);
+        c.gridy = 3;
         frame.add(myButton, c);
     }
 
@@ -50,6 +90,13 @@ public class MainScreen implements ActionListener {
           
             GameScreen myScreen = new GameScreen(20,10,20);
             frame.dispose();
+        }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if(e.getSource() == rSlider){
+            rowLabel.setText("Rows: " + rSlider.getValue());
         }
     }
 }
